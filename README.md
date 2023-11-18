d0cbd29cc6862653195f2e7361f6aef5a9d7fb3f
KKaged <dhigaredaleon707@gmail.com>
2023-11-18 15:55:28 +0900

    Styled the page, search works

---
+++ README.md
@@ -0,0 +1,2 @@
+This will be a basic website that uses the TV Maze TV API to search for a TV show.
+This will display images and the shows information.
---
+++ index.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("#searchForm");
+const main = document.querySelector("#main");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  form.elements.query.value = "";
+  displaySearch(res.data);
+});
+
+const displaySearch = (shows) => {
+  for (let results of shows) {
+    const card = document.createElement("div");
+    card.id = "card-container";
+    const img = document.createElement("img");
+    img.className = "prevImg";
+    img.src = results.show.image.medium;
+    const desc = document.createElement("div");
+    desc.id = "desc";
+    const showName = document.createElement("h2");
+    showName.id = "showName";
+    showName.innerText = results.show.name;
+    const avgRating = document.createElement("h3");
+    avgRating.id = "avgRating";
+    avgRating.innerText = "Rating Average: " + results.show.rating.average;
+    const status = document.createElement("p");
+    status.id = "status";
+    status.innerText = "Status: " + results.show.status;
+    const avgRun = document.createElement("p");
+    avgRun.id = "avgRun";
+    avgRun.innerText =
+      "Average Runtime: " + results.show.averageRuntime + " minutes";
+    const genre = document.createElement("p");
+    genre.id = "genre";
+    genre.innerText = "Genres: " + results.show.genres;
+
+    main.appendChild(card);
+    card.appendChild(img);
+    desc.appendChild(showName);
+    card.append(desc);
+    desc.appendChild(avgRating);
+    desc.appendChild(status);
+    desc.appendChild(avgRun);
+    desc.appendChild(genre);
+  }
+};
+const createCard = () => {};
